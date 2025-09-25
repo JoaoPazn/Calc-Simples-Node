aed44d2c775f268e7165f046edfa5fb9c4128579
Joao Paz <joaovitor223344joao@gmail.com>
2025-09-25 11:07:51 -0300

    Implementado o Requisito para ser número inteiro ou não

--- Calc-Simples-Node/Server.js
+++ Calc-Simples-Node/Server.js
@@ -8,37 +8,46 @@ app.get('/', (req,res) =>{
 app.get('/soma/:A/:B', (req,res) =>{
     let A = parseInt(req.params.A);
     let B = parseInt(req.params.B);
-
+    if(Number.isInteger(A) && Number.isInteger(B)){
     let Sum = A + B;
-
     res.send("o resultado da soma é : " + Sum)
+    }else{
+        res.send("Um dos valores não é um número inteiro.")
+    }
 });
 
 app.get('/subtracao/:A/:B', (req,res) =>{
     let A = parseInt(req.params.A);
     let B = parseInt(req.params.B);
+    if(Number.isInteger(A) && Number.isInteger(B)){
+    let sub = A - B;
+    res.send("o resultado da subtração é : " + sub)
+    }else{
+        res.send("Um dos valores não é um número inteiro.")
+    }
 
-    let Sub = A - B;
-
-    res.send("o resultado da subtracao é : " + Sub)
 });
 
 app.get('/mult/:A/:B', (req,res) =>{
     let A = parseInt(req.params.A);
     let B = parseInt(req.params.B);
-
+    if(Number.isInteger(A) && Number.isInteger(B)){
     let mul = A * B;
-
-    res.send("o resultado da multiplicacao é : " + mul)
+    res.send("o resultado da multiplicação é : " + mul)
+    }else{
+        res.send("Um dos valores não é um número inteiro.")
+    }
 });
 
 app.get('/divi/:A/:B', (req,res) =>{
     let A = parseInt(req.params.A);
     let B = parseInt(req.params.B);
-
+    if(Number.isInteger(A) && Number.isInteger(B)){
     let div = A / B;
-
-    res.send("o resultado da divisao é : " + div)
+    res.send("o resultado da divisão é : " + div)
+    }else{
+        res.send("Um dos valores não é um número inteiro.")
+    }
 });
 
 app.listen(3000, () => {
